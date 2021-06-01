5c8ad28a2b63ebd40f0eea4718724c89c6942748
k <19840104@qq.com>
2021-06-01 22:40:58 +0800

    upgrade seata to 1.4.2

--- kuark-server-seata/build.gradle.kts
+++ kuark-server-seata/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
 //    implementation("io.seata:seata-all")
-    implementation("io.seata:seata-server:1.3.0")
+    implementation("io.seata:seata-server:1.4.2")
 //    implementation("io.seata:seata-spring-boot-starter:1.3.0")
 }
